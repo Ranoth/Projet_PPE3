769e5698eaae1dd7f4ef81b0d3084c17b48cf7d2
Ranoth <47944849+Ranoth@users.noreply.github.com>
2021-12-08 02:56:30 +0100

    Success! (CSV fixtures)

--- src/DataFixtures/MedocFixtures.php
+++ src/DataFixtures/MedocFixtures.php
@@ -2,18 +2,28 @@
 
 namespace App\DataFixtures;
 
-use App\Serializer\CsvEncoder;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Medoc;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class MedocFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        $medoc = new Medoc();
-        $medoc->setNom(CsvEncoder->$array = , 'csv', $context)
+        $serializer = new Serializer([new ObjectNormalizer()], [new CsvEncoder()]);
+
+        $lesMedocs = $serializer->decode(file_get_contents("public/csv/medoc.csv"), 'csv');
+
+        foreach ($lesMedocs as $value) {
+            $medoc = new Medoc();
+            $medoc->setNom(implode($value));
+
+            $manager->persist($medoc);
+        }
 
         $manager->flush();
     }
